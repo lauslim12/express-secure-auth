eeb3d51d267c17c2e75aa38e8e5effa0b1607867
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-23 17:11:37 +0700

    Create infrastructures

---
+++ src/infra/express.ts
@@ -0,0 +1,154 @@
+import compression from 'compression';
+import type { Application, NextFunction, Request, Response } from 'express';
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import type { WrappedNodeRedisClient } from 'handy-redis';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import RedisStore from 'rate-limit-redis';
+
+import AuthHandler from '../auth/handler';
+import AuthMiddleware from '../auth/middleware';
+import AuthRepository from '../auth/repository';
+import AuthService from '../auth/service';
+import MiscHandler from '../misc/handler';
+import UserHandler from '../user/handler';
+import UserMiddleware from '../user/middleware';
+import UserRepository from '../user/repository';
+import UserService from '../user/service';
+import AppError from '../util/appError';
+
+/**
+ * Loads all of the middlewares, routes, and stuff for Express.js.
+ *
+ * @param app - Express application
+ * @param redis - Redis database
+ * @returns Loaded Express application
+ */
+const loadExpress = (app: Application, redis: WrappedNodeRedisClient) => {
+  // Load middlewares.
+  app.use(express.json({ type: 'application/json', limit: '512b' }));
+  app.use(helmet());
+  app.use(hpp());
+  app.use(compression());
+
+  // Setup logger for safety.
+  app.use(morgan('combined'));
+
+  // Prevent XST atttacks by filtering allowed methods.
+  app.use((req: Request, _: Response, next: NextFunction) => {
+    const allowedMethods = [
+      'OPTIONS',
+      'HEAD',
+      'CONNECT',
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE',
+    ];
+
+    if (!allowedMethods.includes(req.method)) {
+      next(new AppError(`Method ${req.method} is not allowed!`, 405));
+      return;
+    }
+
+    next();
+  });
+
+  // Set up general limiter.
+  const limiter = rateLimit({
+    store: new RedisStore({
+      client: redis.nodeRedis,
+      prefix: 'rl-common',
+    }),
+    max: 50,
+    windowMs: 15 * 60 * 1000,
+    handler(_: Request, __: Response, next: NextFunction) {
+      next(new AppError('Too many requests! Please try again later!', 429));
+    },
+  });
+
+  // Set up specific limiter.
+  const strictLimiter = rateLimit({
+    store: new RedisStore({
+      client: redis.nodeRedis,
+      prefix: 'rl-sensitive',
+    }),
+    max: 10,
+    windowMs: 5 * 60 * 1000,
+    handler(_: Request, __: Response, next: NextFunction) {
+      next(
+        new AppError(
+          'Too many requests for this endpoint! Please try again later!',
+          429
+        )
+      );
+    },
+  });
+
+  // Define repositories.
+  const authRepository = new AuthRepository(redis);
+  const userRepository = new UserRepository(redis);
+
+  // Define services.
+  const authService = new AuthService(userRepository, authRepository);
+  const userService = new UserService(userRepository);
+
+  // Define middlewares.
+  const authMiddleware = new AuthMiddleware(authService, userService);
+  const userMiddleware = new UserMiddleware();
+
+  // Define handlers by constructing them with our services.
+  const authHandler = AuthHandler(authService, userService, authMiddleware);
+  const miscHandler = MiscHandler();
+  const userHandler = UserHandler(userService, authMiddleware, userMiddleware);
+
+  // Define API routes.
+  app.use('/', limiter, miscHandler);
+  app.use('/api/v1/authentication', strictLimiter, authHandler);
+  app.use('/api/v1/users', limiter, userHandler);
+
+  // Allow 404.
+  app.all('*', (req: Request, _: Response, next: NextFunction) => {
+    next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
+  });
+
+  // Configure error handlers.
+  app.use((err: AppError, _: Request, res: Response, next: NextFunction) => {
+    if (err instanceof SyntaxError) {
+      err.message = 'Invalid JSON! Please insert a valid one.';
+      err.statusCode = 400;
+      err.isOperational = true;
+    }
+
+    if (err.type === 'entity.too.large') {
+      err.message = 'Request body is too large! Please insert a smaller one.';
+      err.statusCode = 413;
+      err.isOperational = true;
+    }
+
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+
+      next();
+      return;
+    }
+
+    console.error(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'An unknown error occured. Please try again later.',
+    });
+
+    next();
+  });
+
+  // Return application.
+  return app;
+};
+
+export default loadExpress;
---
+++ src/infra/redis.ts
@@ -0,0 +1,23 @@
+import { createNodeRedisClient } from 'handy-redis';
+
+/**
+ * Creates a Redis instance to be used by the application.
+ *
+ * @returns Redis instance
+ */
+const redis = createNodeRedisClient({
+  host: process.env.NODE_ENV === 'production' ? 'redis' : 'localhost',
+});
+
+/**
+ * Set up pub/sub listeners.
+ */
+redis.nodeRedis.on('error', (err) =>
+  console.error('An error occurred when setting up Redis. Error:', err)
+);
+redis.nodeRedis.on('connect', () =>
+  console.log('Successfully connected to the Redis instance!')
+);
+redis.nodeRedis.on('ready', () => console.log('Redis is now ready to work!'));
+
+export default redis;
