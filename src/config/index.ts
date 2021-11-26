10b854e9b7ca10ef344cf1b5c46fb7aeaa7992aa
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-26 21:12:06 +0700

    Replace 'bcrypt' with 'argon2'

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@
 const config = {
   JWT_SECRET: process.env.JWT_SECRET || 'replace with something more random',
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PASSWORD_ALGORITHM: process.env.PASSWORD_ALGORITHM || 'bcrypt',
+  PASSWORD_ALGORITHM: process.env.PASSWORD_ALGORITHM || 'argon2',
   PASSWORD_PEEPER: process.env.PASSWORD_PEEPER || 'randompeeper',
   PORT: process.env.PORT || 8080,
   SESSION_SECRET: process.env.SESSION_SECRET || 'randomsession',
