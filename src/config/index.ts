c0e8aa9af47a8c2ae56fd91c1a6eee1e6da9574b
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-26 14:15:09 +0700

    Add more environment variables and add its loader

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Configure all environment variables for the application.
+ */
+const config = {
+  JWT_SECRET: process.env.JWT_SECRET || 'replace with something more random',
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  PASSWORD_ALGORITHM: process.env.PASSWORD_ALGORITHM || 'bcrypt',
+  PASSWORD_PEEPER: process.env.PASSWORD_PEEPER || 'randompeeper',
+  PORT: process.env.PORT || 8080,
+  SESSION_SECRET: process.env.SESSION_SECRET || 'randomsession',
+};
+
+export default config;
