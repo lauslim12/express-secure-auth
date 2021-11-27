e247dfa1bf9d0f183dfa6f768c2ed59c4f108a02
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-27 09:34:51 +0700

    Refactor password generation and verification

--- src/auth/service.ts
+++ src/auth/service.ts
@@ -5,9 +5,9 @@ import { nanoid } from 'nanoid/async';
 import config from '../config';
 import { verifyArgon2Hash } from '../crypto/argon2';
 import { verifyBCRYPTHash } from '../crypto/bcrypt';
-import createPBKDF2Hash from '../crypto/pbkdf2';
+import { verifyPBKDF2 } from '../crypto/pbkdf2';
 import safeCompare from '../crypto/safeCompare';
-import createSCRYPTHash from '../crypto/scrypt';
+import { verifySCRYPTHash } from '../crypto/scrypt';
 import createSHA512HMAC from '../crypto/sha512';
 import type { Repository as UserRepo } from '../user/domain';
 import type { Auth, Repository as AuthRepo, Service } from './domain';
@@ -53,61 +53,6 @@ const generateJWT = async (
 };
 
 /**
- * Compares passwords that have no need for manual salt.
- *
- * @param checked - A password from the database
- * @param input - A password from the user
- * @returns Boolean value whether passwords match or not
- */
-const compareAutoSaltPasswords = async (checked: string, input: string) => {
-  if (config.PASSWORD_ALGORITHM === 'argon2') {
-    return await verifyArgon2Hash(checked, input);
-  }
-
-  return await verifyBCRYPTHash(checked, input);
-};
-
-/**
- * Compares passwords that have to have manual salt.
- * For manual salts, we have to create a representation of that password in the form of the algorithm.
- * After that, we can compare.
- *
- * @param checked - A password from the database
- * @param input - A password from the user
- * @param salt - A salt from the database
- * @returns Boolean value whether passwords match or not
- */
-const compareManualSaltPasswords = async (
-  checked: string,
-  input: string,
-  salt: string
-) => {
-  if (config.PASSWORD_ALGORITHM === 'pbkdf2') {
-    const userInput = await createPBKDF2Hash(input, salt);
-    const isPasswordValid = safeCompare(userInput, checked);
-    if (isPasswordValid) {
-      return true;
-    }
-
-    return false;
-  }
-
-  if (config.PASSWORD_ALGORITHM === 'scrypt') {
-    const userInput = await createSCRYPTHash(input, salt);
-    const isPasswordValid = safeCompare(userInput, checked);
-    if (isPasswordValid) {
-      return true;
-    }
-
-    return false;
-  }
-
-  // compare sha512
-  const passwordInput = createSHA512HMAC(input, salt);
-  return safeCompare(checked, passwordInput);
-};
-
-/**
  * Compares whether passwords match or not depending on the current used algorithm.
  *
  * @param checked - A password from the database
@@ -120,12 +65,25 @@ const comparePasswords = async (
   input: string,
   salt: string
 ) => {
-  const { PASSWORD_ALGORITHM } = config;
-  if (PASSWORD_ALGORITHM === 'argon2' || PASSWORD_ALGORITHM === 'bcrypt') {
-    return await compareAutoSaltPasswords(checked, input);
+  const { PASSWORD_ALGORITHM: algorithm } = config;
+  if (algorithm === 'argon2') {
+    return await verifyArgon2Hash(checked, input);
   }
 
-  return await compareManualSaltPasswords(checked, input, salt);
+  if (algorithm === 'bcrypt') {
+    return await verifyBCRYPTHash(checked, input);
+  }
+
+  if (algorithm === 'pbkdf2') {
+    return await verifyPBKDF2(checked, salt, input);
+  }
+
+  if (algorithm === 'scrypt') {
+    return await verifySCRYPTHash(checked, salt, input);
+  }
+
+  const passwordInput = createSHA512HMAC(input, salt);
+  return safeCompare(checked, passwordInput);
 };
 
 /**
