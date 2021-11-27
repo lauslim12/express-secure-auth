023cdaffa928dcadfd1c5636453fd76e56fa2772
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-27 09:32:31 +0700

    Add verification function for PBKDF2 and SCRYPT

--- src/crypto/pbkdf2.ts
+++ src/crypto/pbkdf2.ts
@@ -1,5 +1,7 @@
 import { pbkdf2 } from 'crypto';
 
+import safeCompare from './safeCompare';
+
 /**
  * Asynchronously generates a PBKDF2 hash.
  * For PBKDF2, recommended iterations are 10000 and derived key length is 64.
@@ -7,18 +9,59 @@ import { pbkdf2 } from 'crypto';
  *
  * @param raw - The raw password
  * @param salt - The salt
+ * @param digest - Digest / algorithm to use
+ * @param iterations - Number of iterations to use
+ * @param keyLength - Key length of the PBKDF2 hash
  * @returns A PBKDF2 hash
  */
-const createPBKDF2Hash = async (raw: string, salt: string): Promise<string> => {
+export const createPBKDF2Hash = async (
+  raw: string,
+  salt: string,
+  digest: string,
+  iterations: number,
+  keyLength: number
+): Promise<string> => {
   return new Promise((resolve, reject) => {
-    pbkdf2(raw.normalize(), salt, 1000000, 64, 'sha512', (err, key) => {
+    pbkdf2(raw.normalize(), salt, iterations, keyLength, digest, (err, key) => {
       if (err) {
         return reject(err);
       }
 
-      return resolve(key.toString('hex'));
+      const hash = key.toString('hex');
+      const kdf = `$pbkdf2$${digest}$${iterations}$${keyLength}$${hash}`;
+      return resolve(kdf);
     });
   });
 };
 
-export default createPBKDF2Hash;
+/**
+ * Verifies whether strings match or not.
+ *
+ * @param checked - A password from the database
+ * @param salt - A salt from the database
+ * @param input - User input
+ * @returns Boolean value whether 'checked' is safely equal with 'input'
+ */
+export const verifyPBKDF2 = async (
+  checked: string,
+  salt: string,
+  input: string
+) => {
+  // will always fail if passing undefined inputs
+  const pieces = checked.split('$');
+  const digest = pieces[2] || 'sha512';
+  const iterations = parseInt(pieces[3] || '1', 10);
+  const keyLength = parseInt(pieces[4] || '1', 10);
+  const hash = pieces[5] || '';
+
+  const userPBKDF2 = await createPBKDF2Hash(
+    input,
+    salt,
+    digest,
+    iterations,
+    keyLength
+  );
+  const userHash = userPBKDF2.split('$')[5] || '';
+
+  return safeCompare(hash, userHash);
+};
--- src/crypto/scrypt.ts
+++ src/crypto/scrypt.ts
@@ -1,22 +1,53 @@
 import { scrypt } from 'crypto';
 
+import safeCompare from './safeCompare';
+
 /**
  * Asynchronously generates an SCRYPT hash.
  *
  * @param raw - The raw password
  * @param salt - The salt
+ * @param keyLength - The key length of the algorithm
  * @returns An SCRYPT hash
  */
-const createSCRYPTHash = async (raw: string, salt: string): Promise<string> => {
+export const createSCRYPTHash = async (
+  raw: string,
+  salt: string,
+  keyLength: number
+): Promise<string> => {
   return new Promise((resolve, reject) => {
-    scrypt(raw.normalize(), salt, 64, (err, key) => {
+    scrypt(raw.normalize(), salt, keyLength, (err, key) => {
       if (err) {
         return reject(err);
       }
 
-      return resolve(key.toString('hex'));
+      const hash = key.toString('hex');
+      const kdf = `$scrypt$${keyLength}$${hash}`;
+      return resolve(kdf);
     });
   });
 };
 
-export default createSCRYPTHash;
+/**
+ * Verifies SCRYPT hash with the input.
+ *
+ * @param checked - A password from the database
+ * @param salt - A salt from the database
+ * @param input - Input from the user
+ * @returns Boolean value whether the SCRPYT hash is correct or not
+ */
+export const verifySCRYPTHash = async (
+  checked: string,
+  salt: string,
+  input: string
+) => {
+  // will always fail is passing undefined inputs
+  const pieces = checked.split('$');
+  const keyLength = parseInt(pieces[2] || '1', 10);
+  const hash = pieces[3] || '';
+
+  const userSCRPYT = await createSCRYPTHash(input, salt, keyLength);
+  const userHash = userSCRPYT.split('$')[3] || '';
+
+  return safeCompare(hash, userHash);
+};
