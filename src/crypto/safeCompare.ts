bad911fa2b0661e1f9661608c2c8095e980d8515
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-28 16:47:24 +0700

    Allow 'safeCompare' to return false if exception happens

--- src/crypto/safeCompare.ts
+++ src/crypto/safeCompare.ts
@@ -2,18 +2,23 @@ import { timingSafeEqual } from 'crypto';
 
 /**
  * Safely compares two strings with 'timingSafeEqual' to prevent timing attacks.
+ * Allows to return 'false' if two strings are not at the same length.
  *
  * @param a - A string
  * @param b - A string
  * @returns A boolean value to check whether both strings are equal or not
  */
 const safeCompare = (a: string, b: string) => {
-  const equal = timingSafeEqual(
-    Buffer.from(a.normalize()),
-    Buffer.from(b.normalize())
-  );
+  try {
+    const equal = timingSafeEqual(
+      Buffer.from(a.normalize()),
+      Buffer.from(b.normalize())
+    );
 
-  return equal;
+    return equal;
+  } catch {
+    return false;
+  }
 };
 
 export default safeCompare;
